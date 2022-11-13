7a0a5edb606390c8382f6e8a31207f4d9e06a5c8
JooChunWook <jha970107@gmail.com>
2022-11-13 23:14:41 +0900

    템플릿 생성

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -1,38 +1,122 @@
-const onLoadList = (post) => {
+const $home = () => {
+	return `
+		<div class="home">
+			<h1>🔳 JooNotion에 오신걸 환영합니다!</h1>
+		</div>
+	`;
+};
+
+const $editPost = ({ title, content }) => {
+	return `
+	<input style="font-size:30px" type="text" name="title" value="${
+		title === null || title === "" ? "제목없음" : title
+	}">
+	<textarea rows="20" name="content">${content === null ? "" : content}</textarea>
+	`;
+};
+
+/**
+ * 포스트 리스트 (사이드바)
+ * @param {*} post
+ * @returns
+ */
+// const $onLoadList = (post) => {
+// 	return `
+// 	<li class="post-list" data-id="${post.id}">
+// 		<div>
+// 			<span class="open-folder icon-right-open"></span>
+// 			<span class="post-title">
+// 				${post.title}
+// 			</span>
+// 		</div>
+// 		<div>
+// 			<button class="delete-page-btn icon-trash"></button>
+// 			<button class="create-page-btn icon-plus"></button>
+// 		</div>
+// 	</li>
+// 	`;
+// };
+const $onLoadList = (post) => {
 	return `
 	<li class="post-list" data-id="${post.id}">
-		<div>
-			<span class="open-folder icon-right-open"></span>
-			<span class="post-title">
-				${post.title}
-			</span>
-		</div>
-		<div>
-			<button class="delete-page-btn icon-trash"></button>
-			<button class="create-page-btn icon-plus"></button>
+		<div class="list-flex">
+			<div style="line-height:18px">
+				<span class="open-folder icon-right-open"></span>
+				<span class="post-title">
+					${post.title}
+				</span>
+			</div>
+			<div class="delete-create-btn-container">
+				<button class="delete-page-btn icon-trash"></button>
+				<button class="create-page-btn icon-plus-1"></button>
+			</div>
 		</div>
 	</li>
 	`;
 };
 
-const createPostModal = () => {
+/**
+ * 포스트 리스트 헤더
+ * @returns
+ */
+const $sideNavHeader = () => {
 	return `
-	<div class="container-modal">
-	<div class="overlay"></div>
-	<div class="modal">
-		<div class="modal-title">
-			<input type="text" name="title" placeholder="제목을 입력하세요."/>
+		<div class="nav-header">
+		🔳 JooNotion
 		</div>
-		<div class="modal-content">
-		<input
-				type="text"
-				name="content"
-				placeholder="내용을 입력하세요."
-			/>
-		</div>
-	</div>
-</div>
 	`;
 };
 
-export { onLoadList, createPostModal };
+/**
+ * 하위 페이지 없음
+ * @returns
+ */
+const $emptyPage = () => {
+	return `
+		<li class="empty">
+			하위 페이지가 없습니다.
+		</li>
+	`;
+};
+
+const $createPostBtn = () => {
+	return `
+		<div>
+			<button class="icon-plus-1">새 페이지 추가</button>
+		</div>
+	`;
+};
+
+/**
+ * 포스트 작성 모달창
+ * @returns	html
+ */
+const $createPostModal = () => {
+	return `
+	<div class="container-modal">
+		<div class="overlay"></div>
+		<div class="modal">
+			<div class="modal-title">
+				<input type="text" name="title" placeholder="제목을 입력하세요."/>
+			</div>
+			<div class="modal-content">
+			<input
+					type="text"
+					name="content"
+					placeholder="내용을 입력하세요."
+				/>
+			</div>
+		</div>
+	</div>
+	`;
+};
+
+export {
+	$home,
+	$editPost,
+	$sideNavHeader,
+	$onLoadList,
+	$emptyPage,
+	$createPostBtn,
+	$createPostModal,
+};
